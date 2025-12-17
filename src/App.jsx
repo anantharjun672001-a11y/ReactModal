8375b3c27c4f483ec59db6b1852a92391422363e
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-17 10:26:12 +0530

    axios fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ProductList from "./Components/ProductList";
 
 const App = () => {
-  const[product,setProduct]=useState([]);
+  const [product, setProduct] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [status, setStatus] = useState(true);
+
+  const handleInc = () => {
+    setStatus(false);
+    setCart((cart) => cart + 1);
+  };
+  const handleDec = () => {
+    setStatus(true);
+    if (cart > 0) {
+      setCart((cart) => cart - 1);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
     try {
-        const response = await axios.get("https://fakestoreapi.com/products");
-        setProduct(response.data);
-
+      const response = await axios.get("https://fakestoreapi.com/products");
+      setProduct(response.data);
     } catch (error) {
       console.log(error);
-      
     }
-  }
+  };
   return (
     <div>
-      <h1>APP</h1>
+      
+      <ProductList
+        product={product}
+        status={status}
+        handleDec={handleDec}
+        handleInc={handleInc}
+      />
     </div>
   );
 };
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const ProductList = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const ProductList = ({ product,status,handleInc,handleDec }) => {
+  return (
+    <div>
+      {product.map((ele,id) => {
+        return (
+          <div key={id}>
+            <h1>{ele.title}</h1>
+            <img src={ele.image} alt="" />
+            <h1>{ele.price}</h1>
+            <p>{ele.description}</p>
+            <p>{ele.category}</p>
+            {status ? (
+              <button onClick={handleInc}>Add To Cart</button>
+            ) : (
+              <button onClick={handleDec}>Remove From Cart</button>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ProductList;
