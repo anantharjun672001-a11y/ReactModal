ac801c6e0d80cb1a84338d39d846e974ab7b203e
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-17 10:04:49 +0530

    axios fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const App = () => {
+  const[product,setProduct]=useState([]);
+
+  const fetchData = async () => {
+    try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setProduct(response.data);
+
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
   return (
     <div>
       <h1>APP</h1>
