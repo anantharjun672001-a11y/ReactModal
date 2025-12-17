dd7c99d6da7dab165891f2122c51f3152dcbd21c
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-17 12:06:01 +0530

    modal completed

--- src/Components/CartModal.jsx
+++ src/Components/CartModal.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const CartModal = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const CartModal = ({ cart, closeModal, removeFromCart }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <button className="close" onClick={closeModal}>X</button>
+        <h2>Cart Items</h2>
+
+        {cart.length === 0 && <p>No items in cart</p>}
+
+        {cart.map(item => (
+          <div key={item.id} className="cart-item">
+            <img src={item.image} alt={item.title} />
+            <div>
+              <p>{item.title}</p>
+              <p>${item.price}</p>
+              <button onClick={() => removeFromCart(item.id)}>
+                Remove
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CartModal;
