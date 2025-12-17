800a433bc772c7bab1d68d861021a478e82e0a27
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-17 10:52:10 +0530

    navbar completed

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="navbar">
+                <h2>Fake Store</h2>
+                <button onClick={openCart}>Cart ({cartCount})</button>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
